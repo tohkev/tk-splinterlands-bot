dba74a1393a76ca7c5906b3efec66b350c23e4c0
Kevin <tohkev@gmail.com>
2022-05-21 10:12:49 -0700

    move guild brawls project to its own

--- guildBrawls/guildBrawlStart.js
+++
@@ -1,7 +0,0 @@
-//*[@id="create-team-btn"]
-
-//*[@id="brawl_my_battles_list"]/div[4]/div/div[2]/button
-//*[@id="brawl_my_battles_list"]/div[5]/div/div[2]/button
-//*[@id="brawl_my_battles_list"]/div[6]/div/div[2]/button
-//*[@id="brawl_my_battles_list"]/div[7]/div/div[2]/button
-//*[@id="brawl_my_battles_list"]/div[8]/div/div[2]/button
