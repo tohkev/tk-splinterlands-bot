0f6bca624ee58bb33e4e51b3407e9b66e84b64e4
Kevin <tohkev@gmail.com>
2023-01-17 15:21:35 -0800

    fixed login page error

--- splinterlandsPage.js
+++ splinterlandsPage.js
@@ -6,15 +6,15 @@ async function login(page, account, password) {
 			page.click("#log_in_button > button")
 		);
 		await page
-			.waitForSelector("input[name=email]")
+			.waitForSelector("input[name='email']")
 			.then(() => page.waitForTimeout(3000))
-			.then(() => page.focus("input[name=email]"))
-			.then(() => page.type("input[name=email]", account))
-			.then(() => page.focus("input[name=password]"))
-			.then(() => page.type("input[name=password]", password))
+			.then(() => page.focus("input[name='email']"))
+			.then(() => page.type("input[name='email']", account))
+			.then(() => page.focus("input[name='password']"))
+			.then(() => page.type("input[name='password']", password))
 
 			// .then(() => page.waitForSelector('#login_dialog_v2 > div > div > div.modal-body > div > div > form > div > div.col-sm-offset-1 > button', { visible: true }).then(() => page.click('#login_dialog_v2 > div > div > div.modal-body > div > div > form > div > div.col-sm-offset-1 > button')))
-			.then(() => page.click("button[type=submit]"))
+			.then(() => page.click("button[type='submit']"))
 			.then(() => page.waitForTimeout(5000))
 			.then(() => page.reload())
 			.then(() => page.waitForTimeout(5000))
